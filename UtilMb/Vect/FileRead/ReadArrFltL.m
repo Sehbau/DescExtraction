8528b26ea38d12065c844341dca746c9dad14591
Sehbau <134377466+Sehbau@users.noreply.github.com>
2024-06-24 21:59:50 +0200

    Delete UtilMb directory

--- UtilMb/Vect/FileRead/ReadArrFltL.m
+++
@@ -1,3 +0,0 @@
-use either ReadArrFltLp (pure)
-    or     ReadArrFltLh (headed)
-
--- UtilMb/Vect/FileRead/ReadDescHead.m
+++
@@ -1,6 +0,0 @@
-function [] = ReadDescHead()
-
-error('Scuzi, this is old version. Use ReadDescAttHead.m | ReadDescFileHead.m');
-
-end
-
